3527584a16d44e5165abb31d91e4eabad51b8c77
shahnawaz <shahnawazkiit1@gmial.com>
2025-05-20 15:53:52 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// const navList = document.querySelectorAll(".nav li a");
+// console.log(navList);
+// navList.forEach(list => {
+//     list.addEventListener("click", function() {
+//         list.classList.add("action");
+//     })
+// });
